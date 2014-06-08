dae71007fe82437e5cdad09232c218532882ebca
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-06-08 15:40:19 +0530

    refactor source files

--- test/usage.js
+++ test/usage.js
@@ -2,39 +2,32 @@ var usage   = require('../');
 var assert  = require('assert');
 
 suite('Usage', function() {
-
-    test('invalid pid', function(done) {
-        
-        usage.lookup(1232323, function(err, result) {
-
-            assert.ok(err.message);
-            done();
-        });
+  test('invalid pid', function(done) {
+    usage.lookup(1232323, function(err, result) {
+      assert.ok(err.message);
+      done();
     });
+  });
 
-    test('valid pid', function(done) {
-
-        usage.lookup(process.pid, function(err, result) {
-
-            assert.ifError(err);
-            assert.ok(result.cpu >= 0);
-            assert.ok(result.memory > 0);
-            done();
-        });
+  test('valid pid', function(done) {
+    usage.lookup(process.pid, function(err, result) {
+      assert.ifError(err);
+      assert.ok(result.cpu >= 0);
+      assert.ok(result.memory > 0);
+      done();
     });
+  });
 
-    test('valid pid - with keepHistory', function(done) {
-
-        var options = { keepHistory: true };
-        usage.lookup(process.pid, options, function(err, result) {
-
-            assert.ifError(err);
-            assert.ok(result.cpu >= 0);
-            assert.ok(result.memory > 0);
-            if(usage.sysinfo.OS == 'linux') {
-                usage.clearHistory();
-            }
-            done();
-        });
+  test('valid pid - with keepHistory', function(done) {
+    var options = { keepHistory: true };
+    usage.lookup(process.pid, options, function(err, result) {
+      assert.ifError(err);
+      assert.ok(result.cpu >= 0);
+      assert.ok(result.memory > 0);
+      if(usage.sysinfo.OS == 'linux') {
+        usage.clearHistory();
+      }
+      done();
     });
+  });
 });
